b24aa29ea060c9474cc094dac0ffd883ac672d39
sabritos <msabrym2004@gmail.com>
2024-11-27 10:17:42 +0200

    initail backend files

---
+++ backend/ArtLink/src/test/java/com/artlink/ArtLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtLinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
