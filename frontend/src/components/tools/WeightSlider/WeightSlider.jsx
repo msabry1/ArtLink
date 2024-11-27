3b5e7fb09d854c8d795e3af7c8c4089a2ecf4e53
Michael Magdy <michaelmagdymagdy@gmail.com>
2024-11-27 04:53:24 +0200

    finished weight slider layout

---
+++ frontend/src/components/tools/WeightSlider/WeightSlider.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import styles from './WeightSlider.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBorderTopLeft } from '@fortawesome/free-solid-svg-icons';
+
+function WeightSlider({ initialWidth, onWidthChange }) {
+  const [width, setWidth] = useState(initialWidth);
+
+  const handleInputChange = (e) => {
+    setWidth(e.target.value);
+  };
+
+  // Trigger the parent's callback whenever the width changes
+  useEffect(() => {
+    onWidthChange(width);
+  }, [width, onWidthChange]);
+
+  return (
+    <div className={styles.container}>
+      <button className={styles.button}><FontAwesomeIcon icon={faBorderTopLeft}/></button>
+      <div className={styles.sliderContainer}>
+        <label htmlFor="weightSlider">Stroke:</label>
+        <input
+          id="weightSlider"
+          type="range"
+          min="1"
+          max="100"
+          value={width}
+          onChange={handleInputChange}
+          className={`${styles.slider}`}
+        />
+        <p>{width}</p>
+      </div>
+    </div>
+  );
+}
+
+export default WeightSlider;
