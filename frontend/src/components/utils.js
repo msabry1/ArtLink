72b6a6520f1cc36306ff0cc2bf9797f05d6d0de8
Michael Magdy <michaelmagdymagdy@gmail.com>
2024-11-29 03:29:37 +0200

    add a function to generate shape IDs

--- frontend/src/components/utils.js
+++ frontend/src/components/utils.js
@@ -3,3 +3,9 @@ import Shapes from './Canvas/Shapes';
 export function isShape(tool) {
   return Object.values(Shapes).includes(tool);
 }
+
+export function generateShapeId(userIdentifier = "user") {
+  const timestamp = Date.now();
+  const randomValue = Math.random().toString(36).substring(2, 8);
+  return `${userIdentifier}_${timestamp}_${randomValue}`;
+}
